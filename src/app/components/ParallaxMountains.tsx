c6affbe83873c02737a6dd8da363dec438045540
peters <petes726@gmail.com>
2024-08-09 16:05:44 -0400

    Trying to get assets/images working on gh pages

--- src/app/components/ParallaxMountains.tsx
+++ src/app/components/ParallaxMountains.tsx
@@ -13,49 +13,49 @@ export default function ParallaxMountains() {
 				<ParallaxBanner className="bannerContainer">
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer7.svg"
+						image="assets/ParLayer7.svg"
 						speed={-140}
 						style={{ translate: '0 1600px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer6.svg"
+						image="assets/ParLayer6.svg"
 						speed={-120}
 						style={{ translate: '0px 1300px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer5.svg"
+						image="assets/ParLayer5.svg"
 						speed={-100}
 						style={{ translate: '0px 1150px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer4.svg"
+						image="assets/ParLayer4.svg"
 						speed={-80}
 						style={{ translate: '0px 900px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer3.svg"
+						image="assets/ParLayer3.svg"
 						speed={-60}
 						style={{ translate: '0px 700px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer2.svg"
+						image="assets/ParLayer2.svg"
 						speed={-40}
 						style={{ translate: '0px 450px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayer1.svg"
+						image="assets/ParLayer1.svg"
 						speed={0}
 						style={{ translate: '0px 0px' }}
 					/>
 					<ParallaxBannerLayer
 						className="bannerLayer"
-						image="/assets/ParLayerBirds.svg"
+						image="assets/ParLayerBirds.svg"
 						speed={200}
 						style={{ translate: '0px 2920px' }}
 					/>
