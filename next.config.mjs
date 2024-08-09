c658ea1dc60f65782d741a4b130b83907df904b3
peters <petes726@gmail.com>
2024-08-09 15:46:34 -0400

    Commenting out basePath to try to remedy images missing from build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: '/my-portfolio',
+	// basePath: '/my-portfolio',
 	output: 'export', // <=== enables static exports
 	reactStrictMode: true,
 };
