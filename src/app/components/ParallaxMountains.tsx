6ba98a59861e2ab7d250c12f7a616422298f471c
peters <petes726@gmail.com>
2024-08-09 12:28:03 -0400

    Initializing and setting up demo page

---
+++ src/app/components/ParallaxMountains.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { Providers } from '../providers';
+
+import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
+
+import '../styles/ParallaxMountains.css';
+
+export default function ParallaxMountains() {
+	return (
+		<div>
+			<Providers>
+				<ParallaxBanner className="bannerContainer">
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer7.svg"
+						speed={-140}
+						style={{ translate: '0 1600px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer6.svg"
+						speed={-120}
+						style={{ translate: '0px 1300px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer5.svg"
+						speed={-100}
+						style={{ translate: '0px 1150px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer4.svg"
+						speed={-80}
+						style={{ translate: '0px 900px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer3.svg"
+						speed={-60}
+						style={{ translate: '0px 700px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer2.svg"
+						speed={-40}
+						style={{ translate: '0px 450px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayer1.svg"
+						speed={0}
+						style={{ translate: '0px 0px' }}
+					/>
+					<ParallaxBannerLayer
+						className="bannerLayer"
+						image="/assets/ParLayerBirds.svg"
+						speed={200}
+						style={{ translate: '0px 2920px' }}
+					/>
+
+					<div className="absolute inset-0 flex items-center justify-center">
+						<h1 className="text-8xl text-white font-thin">Hello World!</h1>
+					</div>
+				</ParallaxBanner>
+			</Providers>
+		</div>
+	);
+}
