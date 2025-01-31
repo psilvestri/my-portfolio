006a84b5bc891b401d20faf951b3916ff9558d74
peters <petes726@gmail.com>
2025-01-31 11:27:16 -0500

    Attempting to repair static images on Heroku by disabling optimization.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
 	// basePath: '/my-portfolio',
 	// output: 'export', // <=== enables static exports
 	reactStrictMode: true,
+	images: {
+		loader: 'custom',
+		path: '', // Define a custom path if needed
+	},
 };
 
 export default nextConfig;
