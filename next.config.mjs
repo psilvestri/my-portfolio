02fdf91cbd67274e94eac12d63e157aef23cbbf8
peters <petes726@gmail.com>
2025-02-04 22:15:48 -0500

    Reverting config changing and attempting to move portfolio screenshot to assets base folder.

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,6 @@ const nextConfig = {
 	// basePath: '/my-portfolio',
 	// output: 'export', // <=== enables static exports
 	reactStrictMode: true,
-	images: {
-		loader: 'custom',
-		path: '', // Define a custom path if needed
-	},
 };
 
 export default nextConfig;
